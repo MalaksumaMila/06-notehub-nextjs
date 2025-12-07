dec3137a762bb33420fae3fb434b8b0061538da5
Mila <malaksumaaa@gmail.com>
2025-12-07 11:35:05 +0100

    add pakete

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -4,6 +4,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import type { FormikHelpers } from 'formik';
 import * as Yup from 'yup';
+// import toast from `react-hot-toast`
 
 import { createNote } from '../../lib/api';
 
@@ -44,9 +45,9 @@ export default function NoteForm({ closeModal }: NoteFormProps) {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['notes'] });
     },
-    onError: () => {
-      toast.error('Error while creating note');
-    },
+    // onError: () => {
+    //   toast.error('Error while creating note');
+    // },
   });
 
   const handleSubmit = (
